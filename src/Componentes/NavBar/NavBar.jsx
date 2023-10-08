90fb3edb12d1ba8ad8b753a51e6dd213fd32cb42
agusbogado97 <agus.y.roger@gmail.com>
2023-10-08 15:45:59 -0300

    Icono + Nav

--- src/Componentes/NavBar/NavBar.jsx
+++ src/Componentes/NavBar/NavBar.jsx
@@ -2,9 +2,10 @@ function NavBar () {
     return (
       <nav>
         <ul>
-          <li>HOME</li>
-          <li>TIENDA</li>
-          <li>CONTACTO</li>
+          <li><a href=""><img src="https://cdn.icon-icons.com/icons2/3150/PNG/512/clothing_shop_icon_192653.png" alt="Icono Ecommerce" width="50px" height="50px" /></a></li>
+          <li><a href="">HOME</a></li>
+          <li><a href="">TIENDA</a></li>
+          <li><a href="">CONTACTO</a></li>
         </ul>
       </nav>
     )
