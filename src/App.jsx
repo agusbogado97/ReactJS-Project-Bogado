9839311415dc9446fa1985c3e3b077e70c6df743
agusbogado97 <agus.y.roger@gmail.com>
2023-11-04 11:08:06 -0300

    csdiojsio

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 /* import { useState } from 'react';  */
 import './App.css'
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+/* import { BrowserRouter, Routes, Route } from 'react-router-dom'; */
 
 import NavBar from './Componentes/NavBar/NavBar';
 import Titulo  from './Componentes/Titulo/Titulo';
@@ -14,11 +14,11 @@ import ItemListContainer from './Componentes/ItemListContainer/ItemListContainer
 function App() {
   return (
     <div className='App'>
-      <BrowserRouter>
+
         <NavBar/>
         <Titulo tituloApp="Tienda de Ropa" subTituloApp="Encontra lo que queres."/>
         <ItemListContainer/>
-      </BrowserRouter>
+
     </div>
   )
 }
